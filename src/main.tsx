174e3214d111bc432e6c27101c5421ff5a586f66
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2026-06-23 22:56:40 +0530

    perf: add web-vitals RUM (LCP/CLS/INP/TTFB reporting)

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import "@/src/styles/globals.css";
 import { router } from "./router";
+import { initVitals } from "./lib/vitals";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
 );
+
+initVitals();
 
 void import("@fontsource/inter/latin-400.css");
 void import("@fontsource/inter/latin-600.css");
